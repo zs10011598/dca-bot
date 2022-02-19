f1868b7bcaab2036d6e706154536ca15631cff36
promerowork <promerowork26@gmail.com>
2022-02-19 01:55:20 -0600

    fix to get prices more frequently in crontab

--- cron_get_prices.sh
+++ cron_get_prices.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python3 /home/ubuntu/get_prices.py > /home/ubuntu/get_price.log
+
+source /home/ubuntu/dca-bot/.env
+python3 /home/ubuntu/dca-bot/get_prices.py > /home/ubuntu/get_price.log
--- get_prices.py
+++ get_prices.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import time
 import requests
 import logging
 
@@ -21,30 +22,34 @@ def main():
 	url = routes['get_price']
 	book = routes['book_btc']
 
-	res = requests.get('{0}?{1}'.format(url, book))
 
-	try:
-		data = res.json()
-		if data['success']:
-			price_row = {
-				'high': data['payload']['high'],
-				'last': data['payload']['last'],
-				'created_at': data['payload']['created_at'],
-				'book': data['payload']['book'],
-				'volume': data['payload']['volume'],
-				'vwap': data['payload']['vwap'],
-				'low': data['payload']['low'],
-				'ask': data['payload']['ask'],
-				'bid': data['payload']['bid'],
-				'change_24': data['payload']['change_24']
-			}
+	for i in range(5):
+		res = requests.get('{0}?{1}'.format(url, book))
 
-			logging.info('Getting price => Exchange {0} - $ {1} - Book {2}' \
-				.format(exchange, price_row['last'], book))
+		try:
+			data = res.json()
+			if data['success']:
+				price_row = {
+					'high': data['payload']['high'],
+					'last': data['payload']['last'],
+					'created_at': data['payload']['created_at'],
+					'book': data['payload']['book'],
+					'volume': data['payload']['volume'],
+					'vwap': data['payload']['vwap'],
+					'low': data['payload']['low'],
+					'ask': data['payload']['ask'],
+					'bid': data['payload']['bid'],
+					'change_24': data['payload']['change_24']
+				}
 
-			write_db_operation('insert_price', price_row)
+				logging.info('Getting price => Exchange {0} - $ {1} - Book {2}' \
+					.format(exchange, price_row['last'], book))
 
-	except Exception as e:
-		logging.error('Getting price => Exchange {0} - Book {1} - Error {2}'.format(exchange, book, str(e)))
+				write_db_operation('insert_price', price_row)
+
+		except Exception as e:
+			logging.error('Getting price => Exchange {0} - Book {1} - Error {2}'.format(exchange, book, str(e)))
+
+		time.sleep(10)
 
 main()
